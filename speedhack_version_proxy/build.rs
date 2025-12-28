11643187f633fcf94e497d626a77e0332a0e908d
Hirtol <git@hirtol.top>
2025-12-28 23:50:04 +0100

    Comment out other forwards

--- speedhack_version_proxy/build.rs
+++ speedhack_version_proxy/build.rs
@@ -1,5 +1,7 @@
 fn main() {
     forward_dll::forward_dll("C:\\Windows\\System32\\version.dll").unwrap();
-    forward_dll::forward_dll("C:\\Windows\\System32\\bcrypt.dll").unwrap();
-    forward_dll::forward_dll("C:\\Windows\\System32\\hid.dll").unwrap();
+    // forward_dll::forward_dll("C:\\Windows\\System32\\bcrypt.dll").unwrap();
+    // forward_dll::forward_dll("C:\\Windows\\System32\\hid.dll").unwrap();
+    // forward_dll::forward_dll("C:\\Windows\\System32\\PROPSYS.dll").unwrap();
+    // forward_dll::forward_dll("../accord.dll").unwrap();
 }
